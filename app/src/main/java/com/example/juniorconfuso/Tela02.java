df9ae22670cb9b18532a56a0c1bf0ead065c976b
Luiz Otavio Exu Tranca Ruas das Almas <tatoxavierpc@gmail.com>
2026-08-04 13:38:22 -0300

    versão final da primeira aula

--- app/src/main/java/com/example/juniorconfuso/Tela02.java
+++ app/src/main/java/com/example/juniorconfuso/Tela02.java
@@ -1,9 +1,6 @@
 package com.example.juniorconfuso;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,25 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-private Button btn;
+public class Tela02 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tela02);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        btn = findViewById(R.id.button);
-        btn.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        startActivity(new Intent(this, Tela02.class));
     }
 }

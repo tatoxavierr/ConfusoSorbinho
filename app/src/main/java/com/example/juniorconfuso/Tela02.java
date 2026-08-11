9c22fb1a5eeea1592fa59f13098b73075345f281
Luiz Exu Otavio <tatoxavierpc@gmail.com>
2026-08-11 13:16:17 -0300

    final da Segunda Aula

--- app/src/main/java/com/example/juniorconfuso/Tela02.java
+++ app/src/main/java/com/example/juniorconfuso/Tela02.java
@@ -1,14 +1,29 @@
 package com.example.juniorconfuso;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.SeekBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Tela02 extends AppCompatActivity {
+import java.util.concurrent.Semaphore;
+
+public class Tela02 extends AppCompatActivity implements MediaPlayer.OnCompletionListener, SeekBar.OnSeekBarChangeListener, Runnable{
+
+    private Toolbar toolbar;
+    private MediaPlayer mediaPlayer;
+    private SeekBar seekBar;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,89 @@ public class Tela02 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(this);
+        handler = new Handler();
+
+
+
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if (id == android.R.id.home){
+            finish();
+        }
+        if(id == R.id.id001){
+            if(mediaPlayer == null){
+                mediaPlayer = MediaPlayer.create(this, R.raw.forrodofarol_quincasmoreira);
+                mediaPlayer.setOnCompletionListener(this);
+                seekBar.setMax(mediaPlayer.getDuration());
+                handler.post(this);
+                mediaPlayer.start();
+            }else if(!mediaPlayer.isPlaying()){
+                mediaPlayer.start();
+            }
+        }
+        if(id == R.id.id003){
+            if(mediaPlayer != null){
+                mediaPlayer.stop();
+                mediaPlayer.release();
+                mediaPlayer= null;
+            }
+        }
+        if(id == R.id.id002){
+            if(mediaPlayer != null && mediaPlayer.isPlaying()){
+                mediaPlayer.pause();
+            }
+        }
+        return false;
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+
+
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        if(mediaPlayer!=null){
+            mediaPlayer.seekTo(seekBar.getProgress());
+        }
+
+    }
+
+    @Override
+    public void run() {
+        if(mediaPlayer!= null)
+        {
+            seekBar.setProgress(mediaPlayer.getCurrentPosition());
+            handler.postDelayed(this, 1000);
+        }
+
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        mp.release();
+        mp = null;
+        seekBar.setProgress(0);
     }
 }
